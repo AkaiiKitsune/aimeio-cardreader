9eedc62e04b985413721ad67cc34bf32a0a8f0b3
KIT! <norththelittlefox@gmail.com>
2024-02-24 10:30:13 +0100

    Added ACR122U support
    This currently only really works well with felica lite cards (amusement IC).
    
    Older cards or different types (Mifare) should work too but their id won't be correct.

---
+++ build64.bat
@@ -0,0 +1,10 @@
+cd /d %~dp0
+
+REM Edit this with the right path to vcvarsall.bat.
+call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x64
+meson setup build64 --buildtype=release
+meson configure build64
+ninja -C build64
+
+mkdir bin
+copy build64\src\aimeio.dll bin\aimeio.dll
